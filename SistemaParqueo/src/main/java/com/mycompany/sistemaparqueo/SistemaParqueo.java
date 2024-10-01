830366b5dd012ed5737573c3af52c5526cf693ca
KevinCalvo <kevinyadir@hotmail.com>
2024-10-01 11:59:14 -0600

    Proyecto y archivos txt

---
+++ SistemaParqueo/src/main/java/com/mycompany/sistemaparqueo/SistemaParqueo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemaparqueo;
+
+/**
+ *
+ * @author kevin
+ */
+public class SistemaParqueo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
